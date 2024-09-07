15c7db1b3f6bba57cb47e8c7ba8ea81b89dcad08
cshaptx4869 <994774638@qq.com>
2024-09-07 09:43:02 +0800

    feat: :sparkles: 集成unplugin自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,30 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 按需自动导入API
+    AutoImport({
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ["vue", "vue-router"],
+      // 在 vue 模板中自动导入
+      vueTemplate: true,
+      // 指定自动导入函数TS类型声明文件路径 (false:关闭自动生成)
+      dts: "src/types/auto-imports.d.ts",
+    }),
+    // 按需自动导入组件
+    Components({
+      // 指定自定义组件位置(默认:src/components)
+      dirs: ["src/components", "src/**/components"],
+      // 指定自动导入组件TS类型声明文件路径 (false:关闭自动生成)
+      dts: "src/types/components.d.ts",
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
